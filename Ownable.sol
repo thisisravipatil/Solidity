a22b75f50cdb81099e7b99e1ed3e79172f1756a5
Ravi Jyotiba Patil <patlrav@amazon.com>
2019-01-31 00:14:50 +0530

    Added ownable component

---
+++ Ownable.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.17;
+
+contract Ownable {
+    
+    struct ProductOwnership {
+        string productId;
+        string owner;
+    }
+    //"100", "Sherlock distributors, Seattle, Washington"
+    
+    mapping(string => Ownable.ProductOwnership) productOwners;
+    
+    function ownAProduct(
+    string _productId, 
+    string _owner
+    ) 
+    public {
+        productOwners[_productId] = Ownable.ProductOwnership({
+            productId: _productId,
+            owner: _owner
+        });
+    }
+    
+    //Returns new owner
+    function changeOwner(
+    string _productId, 
+    string _newOwner
+    ) 
+    public returns(string){
+        delete productOwners[_productId];
+        
+        productOwners[_productId] = Ownable.ProductOwnership({
+            productId: _productId,
+            owner: _newOwner
+        });
+    }
+    
+    function whoOwns(string _productId) public constant returns (string){
+        return productOwners[_productId].owner;
+    }
+    
+    
+}
